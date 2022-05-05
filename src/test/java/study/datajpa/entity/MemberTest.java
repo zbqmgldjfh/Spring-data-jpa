55c2bfc2aa63c4a3fc312ad041596dcbf6354751
jiwoo <zbqmgldjfh@gmail.com>
2022-05-05 22:08:46 +0900

    feat: Auditing 기능 사용하기
    - BaseTimeEntity, BaseEntity 를 통한 등록시간, 수정시간, 등록자, 수정자 추가하기

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,15 +1,19 @@
 package study.datajpa.entity;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,6 +23,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -48,5 +55,25 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("member.team = " + member.getTeam());
         }
+    }
+    
+    @Test
+    public void JpaEventBaseEntity() throws InterruptedException {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist 발생
+
+        // when
+        Thread.sleep(100);
+        member.setUsername("member2");
+        em.flush();
+        em.clear();
+        
+        // then
+        Member findMember = memberRepository.findById(member.getId()).get();
+        System.out.println("findMember.getCreatedDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdatedDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreatedBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.getLastModifiedBy = " + findMember.getLastModifiedBy());
     }
 }
